ed0ad57840c28cdad1f6fcc4430551018c08ec3f
slayerderk <slayerderk@gmail.com>
2018-08-22 16:15:56 -0500

    Initial commit

---
+++ app/Persona.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{   
+    protected $fillable = ['nombre','tipo_documento','num_documento','direccion','telefono','email'];
+
+    public function provedor()
+    {   //hace referencia q una PERSONA esta relacionada con un solo PROVEEDOR
+        return $this->hasOne('App\Proveedor');
+    }
+
+    public function user()
+    {   //hace referencia q una PERSONA esta relacionada con un solo PROVEEDOR
+        return $this->hasOne('App\User');
+    }
+
+
+}
